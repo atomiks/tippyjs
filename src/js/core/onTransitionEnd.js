4247ea574d84d75a63d7dca6eac7a1cdd9d92b5f
atomiks <na@gmail.com>
2017-09-09 15:49:32 +1000

    Fix transitionend glitch

--- src/js/core/onTransitionEnd.js
+++ src/js/core/onTransitionEnd.js
@@ -1,38 +1,43 @@
 import { Selectors } from './globals'
 
+import getInnerElements from '../utils/getInnerElements'
+
 /**
 * Prepares the callback functions for `show` and `hide` methods
-* @param {Object} refData -  the element/popper reference data
+* @param {Object} data
 * @param {Number} duration
 * @param {Function} callback - callback function to fire once transitions complete
 */
-export default function onTransitionEnd(refData, duration, callback) {
+export default function onTransitionEnd(data, duration, callback) {
   // Make callback synchronous if duration is 0
   if (!duration) {
     return callback()
   }
 
-  const tooltip = refData.popper.querySelector(Selectors.TOOLTIP)
+  const { tooltip } = getInnerElements(data.popper)
+
   let transitionendFired = false
 
   const listenerCallback = e => {
-    if (e.target !== tooltip) return
-
     transitionendFired = true
 
     tooltip.removeEventListener('webkitTransitionEnd', listenerCallback)
     tooltip.removeEventListener('transitionend', listenerCallback)
 
-    callback()
+    if (e.target === tooltip) {
+      callback()
+    }
   }
 
-  // Wait for transitions to complete
+  // Fire callback upon transition completion
   tooltip.addEventListener('webkitTransitionEnd', listenerCallback)
   tooltip.addEventListener('transitionend', listenerCallback)
 
-  // transitionend listener sometimes may not fire
-  clearTimeout(refData._transitionendTimeout)
-  refData._transitionendTimeout = setTimeout(() => {
-    !transitionendFired && callback()
+  // Fallback: transitionend listener sometimes may not fire
+  clearTimeout(data._transitionendTimeout)
+  data._transitionendTimeout = setTimeout(() => {
+    if (!transitionendFired) {
+      callback()
+    }
   }, duration)
 }
