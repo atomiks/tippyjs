a9ccde2df8f8b153b1562a01d49fe8491dd777a0
atomiks <na@gmail.com>
2017-10-06 11:24:38 +1100

    Fix scripts

--- scripts/rollup.standalone.config.js
+++ scripts/rollup.standalone.config.js
@@ -7,22 +7,22 @@ import autoprefixer from 'autoprefixer'
 import cssnano from 'cssnano'
 
 export default Object.assign(base, {
-    dest: './dist/tippy.standalone.js',
-    external: ['popper.js'],
-    plugins: [
-        sass({
-            output: './dist/tippy.css',
-            processor: css => postcss([autoprefixer, cssnano])
-            .process(css)
-            .then(result => result.css)
-        }),
-        babel({
-            presets: ['es2015-rollup'],
-            plugins: ['transform-object-rest-spread', 'transform-object-assign'],
-            exclude: 'node_modules/**',
-        }),
-        babili({
-            comments: false
-        })
-    ]
+  dest: './dist/tippy.standalone.js',
+  external: ['popper.js'],
+  plugins: [
+    sass({
+      output: './dist/tippy.css',
+      processor: css => postcss([autoprefixer, cssnano])
+      .process(css)
+      .then(result => result.css)
+    }),
+    babel({
+      presets: ['es2015-rollup'],
+      plugins: ['transform-object-rest-spread', 'transform-object-assign'],
+      exclude: 'node_modules/**',
+    }),
+    babili({
+      comments: false
+    })
+  ]
 })
