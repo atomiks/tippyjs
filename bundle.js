4594bbd7e84caa473c90e409f59ab008fccee7eb
atomiks <cc.glows@gmail.com>
2018-01-28 13:30:14 +1100

    Remove root dir build files

--- bundle.js
+++
@@ -1,4 +0,0 @@
-import './src/scss/tippy.scss'
-import tippy from './src/js/tippy.js'
-
-export default tippy
--- main.js
+++
@@ -1,7 +0,0 @@
-import styles from './dist/tippy.css'
-import tippy from './src/js/tippy'
-import injectCSS from './src/js/utils/injectCSS'
-
-injectCSS(styles)
-
-export default tippy
