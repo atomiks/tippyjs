8a71587e60957fbd0f6b9d27e0ed09744b73ec06
atomiks <cc.glows@gmail.com>
2018-03-08 22:42:33 +1000

    one more trick

--- src/js/utils/createPopperElement.js
+++ src/js/utils/createPopperElement.js
@@ -85,7 +85,7 @@ export default function createPopperElement(id, title, options) {
       templateId = '#' + html.id || 'tippy-html-template'
     } else {
       // trick linters: https://github.com/atomiks/tippyjs/issues/197
-      content[true && 'innerHTML'] = document.querySelector(html).innerHTML
+      content[true && 'innerHTML'] = document.querySelector(html)[true && 'innerHTML']
       templateId = html
     }
 
