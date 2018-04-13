87ac01b55b6675096369d1303c06e4dac1bf3f93
jburghardt <10438767+jburghardt@users.noreply.github.com>
2018-04-13 10:06:32 +0200

    fix #221: do not show on disabled elements in FF (#222)
    * fix #221 do not show on disabled elements in FF
    
    fix for FF as described in #221
    
    * fix #221 use hasAttribute()

--- src/js/core/Tippy.js
+++ src/js/core/Tippy.js
@@ -73,7 +73,10 @@ export class Tippy {
     // to be created with an empty title. Make sure that the tooltip
     // content is not empty before showing it
     if (options.dynamicTitle && !reference.getAttribute('data-original-title')) return
-
+    
+    // Do not show tooltip if reference contains 'disabled' attribute. FF fix for #221
+    if (reference.hasAttribute('disabled')) return
+    
     // Destroy tooltip if the reference element is no longer on the DOM
     if (!reference.refObj && !document.documentElement.contains(reference)) {
       this.destroy()
