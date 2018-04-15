201ff0b18432af2bafdc7f7cf5858553a239144f
atomiks <cc.glows@gmail.com>
2018-04-15 17:22:40 +1000

    add: more option tests

--- tests/spec/options.test.js
+++ tests/spec/options.test.js
@@ -11,64 +11,139 @@ const o = options => ({ createPopperInstanceOnInit: true, ...options })
 
 afterEach(cleanDocument)
 
-test('createPopperInstanceOnInit', () => {
-  const el = createReferenceElement()
-  tippy(el, { createPopperInstanceOnInit: true })
-  expect(el._tippy.options.createPopperInstanceOnInit).toBe(true)
-  expect(el._tippy.popperInstance).not.toBeNull()
-  expect(el._tippy.popperInstance.state.eventsEnabled).toBe(false)
-})
+describe('createPopperInstanceOnInit', () => {
+  test('creates the popper instance on init if `true`', () => {
+    const tip = tippy.one(createReferenceElement(), {
+      createPopperInstanceOnInit: true
+    })
+    expect(tip.options.createPopperInstanceOnInit).toBe(true)
+    expect(tip.popperInstance).not.toBeNull()
+  })
 
-test('placement', () => {
-  const placements = ['top', 'bottom', 'left', 'right'].reduce(
-    (acc, placement) => [...acc, placement, `${placement}-start`, `${placement}-end`],
-    []
-  )
-  placements.forEach(placement => {
-    const el = createReferenceElement()
-    tippy(el, o({ placement }))
-    expect(el._tippy.options.placement).toBe(placement)
-    expect(el._tippy.popperInstance.options.placement).toBe(placement)
+  test('does not create the popper instance if `false`', () => {
+    const tip = tippy.one(createReferenceElement(), {
+      createPopperInstanceOnInit: false
+    })
+    expect(tip.options.createPopperInstanceOnInit).toBe(false)
+    expect(tip.popperInstance).toBeNull()
+  })
+
+  test('popper event listeners are disabled on init if `true`', () => {
+    const tip = tippy.one(createReferenceElement(), {
+      createPopperInstanceOnInit: true
+    })
+    expect(tip.popperInstance.state.eventsEnabled).toBe(false)
   })
 })
 
-test('trigger', () => {
-  const trigger = 'mouseenter focus click'
-  let el
-
-  el = createReferenceElement(true)
-  tippy(el, o({ trigger }))
-  expect(el._tippy.options.trigger).toBe(trigger)
-  el.dispatchEvent(new Event('mouseenter'))
-  expect(el._tippy.state.visible).toBe(true)
-
-  cleanDocument()
-
-  el = createReferenceElement(true)
-  tippy(el, o({ trigger }))
-  el.dispatchEvent(new Event('focus'))
-  expect(el._tippy.state.visible).toBe(true)
-
-  cleanDocument()
-
-  el = createReferenceElement(true)
-  tippy(el, o({ trigger }))
-  el.dispatchEvent(new Event('click'))
-  expect(el._tippy.state.visible).toBe(true)
+describe('placement', () => {
+  test('placement is in popperInstance options', () => {
+    const tip = tippy.one(createReferenceElement(), o({ placement: 'left' }))
+    expect(tip.popperInstance.options.placement).toBe('left')
+  })
 })
 
-test('allowTitleHTML', () => {
-  const el = createReferenceElement()
-  el.title = '<strong>tooltip</strong>'
+describe('trigger', () => {
+  test('mouseenter', () => {
+    const tip = tippy.one(
+      createReferenceElement(true),
+      o({
+        trigger: 'mouseenter'
+      })
+    )
+    tip.reference.dispatchEvent(new Event('mouseenter'))
+    expect(tip.state.visible).toBe(true)
+    tip.reference.dispatchEvent(new Event('mouseleave'))
+    expect(tip.state.visible).toBe(false)
+    cleanDocument()
+  })
 
-  tippy(el, { allowTitleHTML: false })
-  expect(
-    el._tippy.popper.querySelector(selectors.CONTENT).querySelector('strong')
-  ).toBeNull()
-  el._tippy.destroy()
+  test('focus', () => {
+    const tip = tippy.one(
+      createReferenceElement(true),
+      o({
+        trigger: 'focus'
+      })
+    )
+    tip.reference.dispatchEvent(new Event('focus'))
+    expect(tip.state.visible).toBe(true)
+    tip.reference.dispatchEvent(new Event('blur'))
+    expect(tip.state.visible).toBe(false)
+    cleanDocument()
+  })
 
-  tippy(el, { allowTitleHTML: true })
-  expect(
-    el._tippy.popper.querySelector(selectors.CONTENT).querySelector('strong')
-  ).not.toBeNull()
+  test('click', () => {
+    const tip = tippy.one(
+      createReferenceElement(true),
+      o({
+        trigger: 'click'
+      })
+    )
+    tip.reference.dispatchEvent(new Event('click'))
+    expect(tip.state.visible).toBe(true)
+    tip.reference.dispatchEvent(new Event('click'))
+    expect(tip.state.visible).toBe(false)
+    cleanDocument()
+  })
+})
+
+describe('allowTitleHTML', () => {
+  test('injects HTML if `true`', () => {
+    const el = createReferenceElement()
+    el.title = '<strong>tooltip</strong>'
+    const tip = tippy.one(el, { allowTitleHTML: true })
+    expect(
+      tip.popper.querySelector(selectors.CONTENT).querySelector('strong')
+    ).not.toBeNull()
+  })
+
+  test('injects textContent if `false`', () => {
+    const el = createReferenceElement()
+    el.title = '<strong>tooltip</strong>'
+    const tip = tippy.one(el, { allowTitleHTML: false })
+    expect(tip.popper.querySelector(selectors.CONTENT).querySelector('strong')).toBeNull()
+  })
+})
+
+describe('arrow', () => {
+  test('creates an arrow element if `true`', () => {
+    const tip = tippy.one(createReferenceElement(), {
+      arrow: true
+    })
+    expect(tip.popper.querySelector(selectors.ARROW)).not.toBeNull()
+  })
+
+  test('does not create an arrow element if `false`', () => {
+    const tip = tippy.one(createReferenceElement(), {
+      arrow: false
+    })
+    expect(tip.popper.querySelector(selectors.ARROW)).toBeNull()
+  })
+
+  test('disables the `animateFill` option if `true`', () => {
+    const tip = tippy.one(createReferenceElement(), {
+      arrow: true
+    })
+    expect(tip.options.animateFill).toBe(false)
+  })
+})
+
+describe('animateFill', () => {
+  test('sets the `data-animatefill` attribute on the tooltip element if `true`', () => {
+    const tip = tippy.one(createReferenceElement(), {
+      animateFill: true
+    })
+    expect(
+      tip.popper.querySelector(selectors.TOOLTIP).hasAttribute('data-animatefill')
+    ).toBe(true)
+  })
+
+  test('does not set the `data-animatefill` attribute on the tooltip element if `false`', () => {
+    const tip = tippy.one(createReferenceElement(), {
+      animateFill: false
+    })
+    expect(
+      tip.popper.querySelector(selectors.TOOLTIP).hasAttribute('data-animatefill')
+    ).toBe(false)
+  })
 })
