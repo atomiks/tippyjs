2b49613c960106c456b19631567ec222a800cb6c
atomiks <cc.glows@gmail.com>
2019-01-05 14:56:28 +1100

    Enable analytics

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -36,7 +36,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    /*
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -44,7 +43,6 @@ module.exports = {
         head: false,
       },
     },
-    */
     `gatsby-plugin-catch-links`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
