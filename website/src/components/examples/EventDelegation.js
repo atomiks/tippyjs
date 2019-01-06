e8714e62244d4921413b0ff69e984479f7785836
atomiks <cc.glows@gmail.com>
2019-01-06 15:31:54 +1100

    Add docs for event delegation

---
+++ website/src/components/examples/EventDelegation.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Tippy from '../Tippy'
+import { Button } from '../Framework'
+
+function EventDelegation() {
+  return (
+    <Tippy target=".child" performance={false}>
+      <div id="parent">
+        <Button className="child" data-tippy-content="Tooltip 1">
+          One
+        </Button>
+        <Button
+          className="child"
+          data-tippy-content="Tooltip 2"
+          data-tippy-arrow="true"
+        >
+          Two
+        </Button>
+        <Button
+          className="child"
+          data-tippy-content="Tooltip 3"
+          data-tippy-theme="light"
+        >
+          Three
+        </Button>
+      </div>
+    </Tippy>
+  )
+}
+
+export default EventDelegation
