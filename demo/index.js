914f5ed8a0d7824264e95236d440e419b9dec4c4
atomiks <cc.glows@gmail.com>
2019-01-24 07:48:28 +1100

    Rewriting demo

--- demo/index.js
+++ demo/index.js
@@ -1,23 +1,7 @@
-import tippy from '../src/js/index'
-import '../src/scss/tippy.scss'
+import tippy from '../src/js'
+import '../src/scss/index.scss'
 import '../src/scss/themes/light-border.scss'
 
 window.tippy = tippy
 
-tippy('button:not([data-exclude]), p', {
-  content: 'I am a Tippy tooltip',
-})
-
-const template = document.createElement('div')
-template.innerHTML = '<strong>tooltip</strong>'
-tippy('.html', {
-  content: template,
-})
-
-tippy('.multiple', {
-  content: 'tooltip',
-})
-tippy('.multiple', {
-  content: '<strong>popover</strong>',
-  trigger: 'click',
-})
+tippy('[data-tippy-content]')
