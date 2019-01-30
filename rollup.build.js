26b5732d1d549b7672acba8685891452158b1da6
atomiks <cc.glows@gmail.com>
2019-01-30 18:55:04 +1100

    'esm' is preferred by Rollup over 'es' now

--- rollup.build.js
+++ rollup.build.js
@@ -52,32 +52,48 @@ const output = format => (file, { min = false } = {}) => {
   }
 }
 
-const bundleFormats = [output('umd'), output('es')]
-const cssFormat = output('css')
-const themesFormat = output('themes')
+const configs = {
+  css: config('./build/css.js'),
+  index: config('./build/index.js'),
+  all: config('./build/all.js'),
+  // plugins: {
+  //   group: config('./build/plugins/group.js'),
+  // },
+}
+
+const outputs = {
+  bundle: [output('umd'), output('esm')],
+  css: output('css'),
+  theme: output('themes'),
+}
 
 const build = async () => {
   console.log(blue('⏳ Building bundles...'))
 
-  const indexConfig = config('./build/index.js')
-  const allConfig = config('./build/all.js')
-
   const preCSSBundle = await rollup(
-    config('./build/css.js')(createPluginSCSS('./index.css')),
+    configs.css(createPluginSCSS('./index.css')),
   )
-  await preCSSBundle.write(cssFormat('./index.js'))
+  await preCSSBundle.write(outputs.css('./index.js'))
   fs.unlinkSync('./index.js')
 
-  const bundle = await rollup(indexConfig())
-  const bundleMin = await rollup(indexConfig(pluginMinify))
-  const bundleCSS = await rollup(allConfig(pluginCSS))
-  const bundleCSSMin = await rollup(allConfig(pluginMinify, pluginCSS))
+  const bundles = {
+    index: await rollup(configs.index()),
+    indexMin: await rollup(configs.index(pluginMinify)),
+    all: await rollup(configs.all(pluginCSS)),
+    allMin: await rollup(configs.all(pluginMinify, pluginCSS)),
+    // plugins: {
+    //   group: await rollup(configs.plugins.group()),
+    //   groupMin: await rollup(configs.plugins.group(pluginMinify)),
+    // },
+  }
 
-  for (const format of bundleFormats) {
-    bundle.write(format('index.js'))
-    bundleMin.write(format('index.min.js', { min: true }))
-    bundleCSS.write(format('index.all.js'))
-    bundleCSSMin.write(format('index.all.min.js', { min: true }))
+  for (const getOutput of outputs.bundle) {
+    bundles.index.write(getOutput('index.js'))
+    bundles.indexMin.write(getOutput('index.min.js'))
+    bundles.all.write(getOutput('index.all.js'))
+    bundles.allMin.write(getOutput('index.all.min.js'))
+    // bundles.plugins.group.write(getOutput('plugins/group.js'))
+    // bundles.plugins.groupMin.write(getOutput('plugins/group.min.js'))
   }
 
   console.log(green('Bundles complete'))
@@ -86,9 +102,9 @@ const build = async () => {
 
   for (const theme of fs.readdirSync('./build/themes')) {
     const themeConfig = config(`./build/themes/${theme}`)
-    const outputFile = `./themes/${theme.replace('.js', '')}.css`
+    const outputFile = `./themes/${theme.replace('.js', '.css')}`
     const bundle = await rollup(themeConfig(createPluginSCSS(outputFile)))
-    await bundle.write(themesFormat(theme))
+    await bundle.write(outputs.theme(theme))
     fs.unlinkSync(`./themes/${theme}`)
   }
 
