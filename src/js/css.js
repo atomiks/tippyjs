0fdb7fdc70795cab0ccf39b00598d40f812f845c
atomiks <cc.glows@gmail.com>
2019-02-05 20:29:35 +1100

    SSR check

--- src/js/css.js
+++ src/js/css.js
@@ -1,10 +1,14 @@
+import { isBrowser } from './browser'
+
 /**
  * Injects a string of CSS styles to a style node in <head>
  * @param {String} css
  */
 export function injectCSS(css) {
-  const style = document.createElement('style')
-  style.type = 'text/css'
-  style.textContent = css
-  document.head.insertBefore(style, document.head.firstChild)
+  if (isBrowser) {
+    const style = document.createElement('style')
+    style.type = 'text/css'
+    style.textContent = css
+    document.head.insertBefore(style, document.head.firstChild)
+  }
 }
