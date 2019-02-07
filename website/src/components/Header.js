ae9a620606e2e4c5585015d7326c288ad95afdcf
atomiks <cc.glows@gmail.com>
2019-02-08 07:58:25 +1100

    Version notice

--- website/src/components/Header.js
+++ website/src/components/Header.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import TippyLogo from '../images/logo.svg'
 import { MEDIA, Container, Flex, ExternalLink } from './Framework'
 import GitHub from 'react-feather/dist/icons/github'
 import CloudLightning from 'react-feather/dist/icons/cloud-lightning'
 import Menu from 'react-feather/dist/icons/menu'
+import VersionNotice from './VersionNotice'
 
 const HeaderRoot = styled.header`
   position: relative;
@@ -80,32 +81,53 @@ const menuStyles = {
   height: 36,
 }
 
-function Header({ openNav, isNavOpen }) {
-  return (
-    <HeaderRoot>
-      <Container>
-        <Logo src={TippyLogo} draggable="false" />
-        <Title>Tippy.js</Title>
-        <Flex justify="center">
-          <ButtonLink href="https://popper.js.org">
-            <CloudLightning style={cloudLightningStyles} />
-            Powered by Popper.js
-          </ButtonLink>
-          <ButtonLink href="https://github.com/atomiks/tippyjs">
-            <GitHub style={githubStyles} />
-            View on GitHub
-          </ButtonLink>
-        </Flex>
-        <MenuButton
-          aria-label="Menu"
-          aria-expanded={isNavOpen ? 'true' : 'false'}
-          onClick={openNav}
-        >
-          <Menu style={menuStyles} />
-        </MenuButton>
-      </Container>
-    </HeaderRoot>
-  )
+class Header extends Component {
+  state = {
+    displayVersionNotice: false,
+  }
+
+  componentDidMount() {
+    // People who visit 'Getting Started' are most likely new, so they don't
+    // need to know about the new version. Also only show it once.
+    const { pageIndex } = this.props
+    const disableVersionNotice = localStorage.getItem('disableVersionNotice')
+    localStorage.setItem('disableVersionNotice', 'true')
+
+    this.setState({
+      displayVersionNotice: pageIndex > 1 && disableVersionNotice === null,
+    })
+  }
+
+  render() {
+    const { isNavOpen, openNav } = this.props
+    const { displayVersionNotice } = this.state
+    return (
+      <HeaderRoot>
+        <Container>
+          {displayVersionNotice && <VersionNotice />}
+          <Logo src={TippyLogo} draggable="false" />
+          <Title>Tippy.js</Title>
+          <Flex justify="center">
+            <ButtonLink href="https://popper.js.org">
+              <CloudLightning style={cloudLightningStyles} />
+              Powered by Popper.js
+            </ButtonLink>
+            <ButtonLink href="https://github.com/atomiks/tippyjs">
+              <GitHub style={githubStyles} />
+              View on GitHub
+            </ButtonLink>
+          </Flex>
+          <MenuButton
+            aria-label="Menu"
+            aria-expanded={isNavOpen ? 'true' : 'false'}
+            onClick={openNav}
+          >
+            <Menu style={menuStyles} />
+          </MenuButton>
+        </Container>
+      </HeaderRoot>
+    )
+  }
 }
 
 export default Header
