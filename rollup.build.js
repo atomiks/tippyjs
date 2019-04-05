6796b36a4c11cfcf5ce80277233ed7b320158921
atomiks <cc.glows@gmail.com>
2019-04-05 20:31:21 +1100

    Update dependencies to use not dead browserslist query

--- rollup.build.js
+++ rollup.build.js
@@ -52,7 +52,7 @@ const createPluginSCSS = output => {
     output,
     processor: css =>
       postcss([autoprefixer, cssnano])
-        .process(css)
+        .process(css, { from: undefined })
         .then(result => result.css),
   })
 }
