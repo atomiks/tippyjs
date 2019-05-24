9f4d62af2b25ed763b6323a1ab12dd3bf16d41f7
atomiks <cc.glows@gmail.com>
2019-05-24 10:26:30 +1000

    Make the interfaces types instead

--- addons/index.d.ts
+++ addons/index.d.ts
@@ -1,18 +1,14 @@
 import { Instance, Targets, Options } from '..'
 
-export interface Delegate {
-  (targets: Targets, options: Options & { target: string }):
-    | Instance
-    | Instance[]
-    | null
-}
+export type Delegate = (
+  targets: Targets,
+  options: Options & { target: string },
+) => Instance | Instance[] | null
 
-export interface CreateSingleton {
-  (
-    tippyInstances: Instance[],
-    options?: { delay: number | [number, number] },
-  ): Instance
-}
+export type CreateSingleton = (
+  tippyInstances: Instance[],
+  options?: { delay: number | [number, number] },
+) => Instance
 
 declare const delegate: Delegate
 declare const createSingleton: CreateSingleton
