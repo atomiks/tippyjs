30505dd0ec2da616ed6957424e2bcdcae5696927
atomiks <cc.glows@gmail.com>
2019-05-28 17:54:45 +1000

    style tag type is optional in HTML5

--- src/css.ts
+++ src/css.ts
@@ -6,7 +6,6 @@ import { isBrowser } from './browser'
 export function injectCSS(css: string): void {
   if (isBrowser) {
     const style = document.createElement('style')
-    style.type = 'text/css'
     style.textContent = css
     style.setAttribute('data-__NAMESPACE_PREFIX__-stylesheet', '')
     const head = document.head
