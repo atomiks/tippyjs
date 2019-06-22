36b882859f9ae0c7fb1c9f2fff8948fa50ed6df5
Carbon Copy <cc.glows@gmail.com>
2019-06-22 15:20:19 +1000

    Improve types

--- src/extra-props/enhance.ts
+++ src/extra-props/enhance.ts
@@ -1,5 +1,29 @@
-import { Tippy } from '../types'
+import { Tippy, Props } from '../types'
 
-export default function enhance(tippyBase: Tippy, propHOFs: Function[]) {
-  return propHOFs.reduce((nextTippy, addProp) => addProp(nextTippy), tippyBase)
+interface EnhancedTippy extends Tippy {
+  version: string
+  defaultProps: Props
+}
+
+type PropHOF = (tippy: Tippy) => EnhancedTippy
+
+function copyStatics(to: EnhancedTippy, from: Tippy): EnhancedTippy {
+  to.version = from.version
+  to.defaultProps = from.defaultProps
+  to.setDefaultProps = from.setDefaultProps
+  to.hideAll = from.hideAll
+
+  return to
+}
+
+export default function enhance(
+  tippyBase: Tippy,
+  propHOFs: PropHOF[],
+): EnhancedTippy {
+  const tippyEnhanced = propHOFs.reduce(
+    (nextTippy, addProp): EnhancedTippy => addProp(nextTippy),
+    tippyBase,
+  )
+
+  return copyStatics(tippyEnhanced, tippyBase)
 }
