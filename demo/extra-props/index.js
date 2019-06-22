5daeaa7b09d6f6536054f915009a3507f984c9a5
Carbon Copy <cc.glows@gmail.com>
2019-06-22 14:55:34 +1000

    Add extra-props demo

---
+++ demo/extra-props/index.js
@@ -0,0 +1,28 @@
+import tippyBase from '../../src'
+import '../../src/scss/index.scss'
+import enhance from '../../src/extra-props/enhance'
+import inlinePositioning from '../../src/extra-props/inlinePositioning'
+import followCursor from '../../src/extra-props/followCursor'
+
+const tippy = enhance(tippyBase, [followCursor, inlinePositioning])
+
+const placements = ['top', 'right', 'bottom', 'left']
+placements.forEach(placement => {
+  const props = {
+    placement,
+    multiple: true,
+    content: 'tooltip',
+    duration: 0,
+    arrow: true,
+  }
+  tippy('.inline', { ...props, inlinePositioning: true })
+  tippy('.cursorRect', { ...props, inlinePositioning: 'cursorRect' })
+  tippy('.cursorPoint', { ...props, inlinePositioning: 'cursorPoint' })
+})
+
+tippy('.followCursor', {
+  content: 'tooltip',
+  animation: 'fade',
+  animateFill: false,
+  followCursor: true,
+})
