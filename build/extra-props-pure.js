3fe2e955b938f306d9d03121f8e9b8dc268cb90d
Carbon Copy <cc.glows@gmail.com>
2019-06-25 03:04:19 +1000

    Add inlinePositioning to extra-props build

--- build/extra-props-pure.js
+++ build/extra-props-pure.js
@@ -1,5 +1,6 @@
 import enhance from '../src/extra-props/enhance.ts'
 import followCursor from '../src/extra-props/followCursor.ts'
+import inlinePositioning from '../src/extra-props/inlinePositioning'
 
 export default enhance
-export { followCursor }
+export { followCursor, inlinePositioning }
--- build/extra-props.js
+++ build/extra-props.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
 import enhance from '../src/extra-props/enhance.ts'
 import followCursor from '../src/extra-props/followCursor.ts'
+import inlinePositioning from '../src/extra-props/inlinePositioning'
 
 if (typeof tippy === 'function') {
-  window.tippy = enhance(tippy, [followCursor])
+  window.tippy = enhance(tippy, [followCursor, inlinePositioning])
 } else {
   throw new Error(
     '[tippy.js ERROR] `tippy` is not a global function. Make sure you have ' +
