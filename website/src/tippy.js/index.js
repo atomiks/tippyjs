fdba9ba5bde76d3a457454a67641427d8bb4d61d
Carbon Copy <cc.glows@gmail.com>
2019-08-09 18:19:57 +1000

    Update temp <Tippy /> wrapper

--- website/src/tippy.js/index.js
+++ website/src/tippy.js/index.js
@@ -3,7 +3,6 @@ import React, {
   forwardRef,
   cloneElement,
   useState,
-  useRef,
   useLayoutEffect,
   useEffect,
   Children,
@@ -12,45 +11,16 @@ import { createPortal } from 'react-dom'
 import tippyBase from '../../../'
 import { createSingleton, delegate } from '../../../addons'
 import enhance, { followCursor } from '../../../extra-props'
+import { useThis } from '../hooks'
+
+const isBrowser =
+  typeof window !== 'undefined' && typeof document !== 'undefined'
 
 const tippy = enhance(tippyBase, [followCursor])
 
-function _extends() {
-  _extends =
-    Object.assign ||
-    function(target) {
-      for (var i = 1; i < arguments.length; i++) {
-        var source = arguments[i]
+createSingleton.tippy = tippy
+delegate.tippy = tippy
 
-        for (var key in source) {
-          if (Object.prototype.hasOwnProperty.call(source, key)) {
-            target[key] = source[key]
-          }
-        }
-      }
-
-      return target
-    }
-
-  return _extends.apply(this, arguments)
-}
-
-function _objectWithoutPropertiesLoose(source, excluded) {
-  if (source == null) return {}
-  var target = {}
-  var sourceKeys = Object.keys(source)
-  var key, i
-
-  for (i = 0; i < sourceKeys.length; i++) {
-    key = sourceKeys[i]
-    if (excluded.indexOf(key) >= 0) continue
-    target[key] = source[key]
-  }
-
-  return target
-}
-
-var isBrowser = typeof window !== 'undefined' && typeof document !== 'undefined'
 function preserveRef(ref, node) {
   if (ref) {
     if (typeof ref === 'function') {
@@ -62,9 +32,11 @@ function preserveRef(ref, node) {
     }
   }
 }
+
 function ssrSafeCreateDiv() {
   return isBrowser && document.createElement('div')
 }
+
 function updateClassName(tooltip, action, classNames) {
   classNames.split(/\s+/).forEach(function(name) {
     if (name) {
@@ -73,170 +45,125 @@ function updateClassName(tooltip, action, classNames) {
   })
 }
 
-// get around it, we can conditionally useEffect on the server (no-op) and
-// useLayoutEffect in the browser. We need useLayoutEffect because we want Tippy
-// to perform sync mutations to the DOM elements after renders to prevent
-// jitters/jumps, especially when updating content.
-
 var useIsomorphicLayoutEffect = isBrowser ? useLayoutEffect : useEffect
 
-function Tippy(_ref) {
-  var children = _ref.children,
-    content = _ref.content,
-    className = _ref.className,
-    onBeforeUpdate = _ref.onBeforeUpdate,
-    onAfterUpdate = _ref.onAfterUpdate,
-    onCreate = _ref.onCreate,
-    isVisible = _ref.isVisible,
-    isEnabled = _ref.isEnabled,
-    visible = _ref.visible,
-    enabled = _ref.enabled,
-    _ref$ignoreAttributes = _ref.ignoreAttributes,
-    ignoreAttributes =
-      _ref$ignoreAttributes === void 0 ? true : _ref$ignoreAttributes,
-    _ref$multiple = _ref.multiple,
-    multiple = _ref$multiple === void 0 ? true : _ref$multiple,
-    restOfNativeProps = _objectWithoutPropertiesLoose(_ref, [
-      'children',
-      'content',
-      'className',
-      'onCreate',
-      'onBeforeUpdate',
-      'onAfterUpdate',
-      'isVisible',
-      'isEnabled',
-      'visible',
-      'enabled',
-      'ignoreAttributes',
-      'multiple',
-    ])
+function Tippy({
+  children,
+  content,
+  className,
+  onBeforeUpdate,
+  onAfterUpdate,
+  onCreate,
+  visible,
+  enabled = true,
+  multiple = true,
+  ignoreAttributes = true,
+  ...restOfNativeProps
+}) {
+  const isControlledMode = visible !== undefined
 
-  // `isVisible` / `isEnabled` renamed to `visible` / `enabled`
-  enabled =
-    enabled !== undefined ? enabled : isEnabled !== undefined ? isEnabled : true
-  visible = visible !== undefined ? visible : isVisible
+  const [mounted, setMounted] = useState(false)
+  const $this = useThis({ container: ssrSafeCreateDiv(), renders: 1 })
 
-  var _useState = useState(false),
-    mounted = _useState[0],
-    setMounted = _useState[1]
-
-  var containerRef = useRef(ssrSafeCreateDiv())
-  var targetRef = useRef()
-  var instanceRef = useRef()
-  var isControlledMode = visible !== undefined
-
-  var options = _extends(
-    {
-      ignoreAttributes: ignoreAttributes,
-      multiple: multiple,
-    },
-    restOfNativeProps,
-    {
-      content: containerRef.current,
-    },
-  )
-
-  if (isControlledMode) {
-    options.trigger = 'manual'
+  const props = {
+    ignoreAttributes,
+    multiple,
+    ...restOfNativeProps,
+    content: $this.container,
   }
 
-  useIsomorphicLayoutEffect(function() {
-    instanceRef.current = tippy(targetRef.current, options)
+  if (isControlledMode) {
+    props.trigger = 'manual'
+  }
+
+  // CREATE
+  useIsomorphicLayoutEffect(() => {
+    const instance = tippy($this.reference, props)
+
+    $this.instance = instance
 
     if (onCreate) {
-      onCreate(instanceRef.current)
+      onCreate(instance)
     }
 
     if (!enabled) {
-      instanceRef.current.disable()
+      instance.disable()
     }
 
     if (visible) {
-      instanceRef.current.show()
+      instance.show()
     }
 
     setMounted(true)
-    return function() {
-      instanceRef.current.destroy()
-      instanceRef.current = null
+
+    return () => {
+      instance.destroy()
     }
   }, [])
-  useIsomorphicLayoutEffect(function() {
-    if (!mounted) {
+
+  // UPDATE
+  useIsomorphicLayoutEffect(() => {
+    // Prevent this effect from running on the initial render, and the render
+    // caused by setMounted().
+    if ($this.renders < 3) {
+      $this.renders++
       return
     }
 
     if (onBeforeUpdate) {
-      onBeforeUpdate(instanceRef.current)
+      onBeforeUpdate($this.instance)
     }
 
-    instanceRef.current.setProps(options)
+    $this.instance.setProps(props)
 
     if (onAfterUpdate) {
-      onAfterUpdate(instanceRef.current)
+      onAfterUpdate($this.instance)
     }
 
     if (enabled) {
-      instanceRef.current.enable()
+      $this.instance.enable()
     } else {
-      instanceRef.current.disable()
+      $this.instance.disable()
     }
 
     if (isControlledMode) {
       if (visible) {
-        instanceRef.current.show()
+        $this.instance.show()
       } else {
-        instanceRef.current.hide()
+        $this.instance.hide()
       }
     }
   })
 
-  useIsomorphicLayoutEffect(
-    function() {
-      if (className) {
-        var tooltip = instanceRef.current.popperChildren.tooltip
-        updateClassName(tooltip, 'add', className)
-        return function() {
-          updateClassName(tooltip, 'remove', className)
-        }
+  // UPDATE className
+  useIsomorphicLayoutEffect(() => {
+    if (className) {
+      const tooltip = $this.instance.popperChildren.tooltip
+      updateClassName(tooltip, 'add', className)
+      return () => {
+        updateClassName(tooltip, 'remove', className)
       }
-    },
-    [className],
-  )
-  return React.createElement(
-    React.Fragment,
-    null,
-    cloneElement(children, {
-      ref: function ref(node) {
-        targetRef.current = node
-        preserveRef(children.ref, node)
-      },
-    }),
-    mounted && createPortal(content, containerRef.current),
+    }
+  }, [className])
+
+  return (
+    <>
+      {cloneElement(children, {
+        ref(node) {
+          $this.reference = node
+          preserveRef(children.ref, node)
+        },
+      })}
+      {mounted && createPortal(content, $this.container)}
+    </>
   )
 }
 
-var Tippy$1 = forwardRef(function TippyWrapper(_ref2, _ref3) {
-  var children = _ref2.children,
-    props = _objectWithoutPropertiesLoose(_ref2, ['children'])
-
-  return React.createElement(
-    Tippy,
-    props,
-    cloneElement(children, {
-      ref: function ref(node) {
-        preserveRef(_ref3, node)
-        preserveRef(children.ref, node)
-      },
-    }),
-  )
-})
-
 function TippySingleton({ children, delay }) {
-  const instancesRef = useRef([])
+  const $this = useThis({ instances: [] })
 
   useEffect(() => {
-    const singleton = createSingleton(instancesRef.current, { delay })
+    const singleton = createSingleton($this.instances, { delay })
     return () => {
       singleton.destroy(false)
     }
@@ -249,11 +176,23 @@ function TippySingleton({ children, delay }) {
           child.props.onCreate(instance)
         }
 
-        instancesRef.current.push(instance)
+        $this.instances.push(instance)
       },
     })
   })
 }
 
-export default Tippy$1
+export default forwardRef(function TippyWrapper({ children, ...props }, ref) {
+  return (
+    <Tippy {...props}>
+      {cloneElement(children, {
+        ref(node) {
+          preserveRef(ref, node)
+          preserveRef(children.ref, node)
+        },
+      })}
+    </Tippy>
+  )
+})
+
 export { TippySingleton, tippy, createSingleton, delegate }
