421d6cdceae6a2c7d3ed1ca76a26b3419d89f91b
Carbon Copy <cc.glows@gmail.com>
2019-08-17 19:57:48 +1000

    Fix: remove error wrapper for IIFE versions
    Will throw a ReferenceError if tippy is not defined due to the IIFE expression anyway

--- build/addons.js
+++ build/addons.js
@@ -2,12 +2,5 @@
 import delegate from '../src/addons/delegate.ts'
 import createSingleton from '../src/addons/createSingleton.ts'
 
-if (typeof tippy === 'function') {
-  tippy.delegate = delegate
-  tippy.createSingleton = createSingleton
-} else {
-  throw new Error(
-    '`tippy` is not a global function. Make sure you have ' +
-      'included the tippy script before tippy-addons.',
-  )
-}
+tippy.delegate = delegate
+tippy.createSingleton = createSingleton
--- build/extra-props.js
+++ build/extra-props.js
@@ -2,11 +2,4 @@
 import enhance from '../src/extra-props/enhance.ts'
 import followCursor from '../src/extra-props/followCursor.ts'
 
-if (typeof tippy === 'function') {
-  window.tippy = enhance(tippy, [followCursor])
-} else {
-  throw new Error(
-    '`tippy` is not a global function. Make sure you have ' +
-      'included the tippy script before tippy-extra-props.',
-  )
-}
+window.tippy = enhance(tippy, [followCursor])
