1bea44ddbea9e23b7489f8837b871fcb79b79e9f
Carbon Copy <cc.glows@gmail.com>
2019-09-17 23:31:54 +1000

    Update deps

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/`,
+        path: `${__dirname}/src/pages`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -48,7 +48,7 @@ module.exports = {
     // 'gatsby-plugin-offline',
     `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-mdx`,
+      resolve: `gatsby-plugin-mdx`,
       options: {
         defaultLayouts: {
           default: require.resolve('./src/components/Layout.js'),
