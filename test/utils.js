2c2420e499245091ec5920b6258f6bc8fd370fd4
Carbon Copy <cc.glows@gmail.com>
2019-09-22 20:28:02 +1000

    Remove unused test util

--- test/utils.js
+++ test/utils.js
@@ -18,10 +18,6 @@ export function cleanDocumentBody() {
   document.body.innerHTML = ''
 }
 
-export function hasTippy(el) {
-  return !!el._tippy
-}
-
 export function h(nodeName = 'button', attributes = {}) {
   const el = document.createElement(nodeName)
   el.className = IDENTIFIER
