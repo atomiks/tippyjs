210d73156a06a38feeeadba411753821ea67b60c
Carbon Copy <cc.glows@gmail.com>
2019-10-19 09:39:23 +1100

    Update website

--- website/src/components/examples/Nesting.js
+++ website/src/components/examples/Nesting.js
@@ -1,47 +1,62 @@
-import React, {useRef} from 'react';
+import React from 'react';
+import styled from 'styled-components';
 import Tippy from '../Tippy';
 import {Button} from '../Framework';
 
-const array = Array(3).fill();
+const Wrapper = styled.div`
+  margin-top: 8px;
+  margin-left: 8px;
+`;
 
 function Nesting() {
-  const parentInstanceRef = useRef();
-  const currentlyOpenNestedTippyRef = useRef();
-
   return (
     <Tippy
       theme="light-border"
       interactive={true}
-      arrow={true}
-      onCreate={instance => (parentInstanceRef.current = instance)}
-      onHide={() => {
-        if (currentlyOpenNestedTippyRef.current) {
-          currentlyOpenNestedTippyRef.current.hide();
-        }
-      }}
       content={
-        <div style={{marginTop: 8, marginLeft: 8}}>
-          {array.map((_, i) => (
-            <Tippy
-              key={i}
-              animation="fade"
-              arrow={true}
-              onShow={instance => {
-                // Prevent showing if parent isn't visible
-                if (!parentInstanceRef.current.state.isVisible) {
-                  return false;
-                } else {
-                  currentlyOpenNestedTippyRef.current = instance;
-                }
-              }}
-            >
-              <Button>Text</Button>
-            </Tippy>
-          ))}
-        </div>
+        <Wrapper>
+          <Tippy
+            theme="light-border"
+            interactive={true}
+            onCreate={({popper}) => {
+              popper.style.width = 'max-content';
+            }}
+            content={
+              <Wrapper>
+                <Tippy
+                  placement="right"
+                  theme="light-border"
+                  interactive={true}
+                  onCreate={({popper}) => {
+                    popper.style.width = 'max-content';
+                  }}
+                  content={
+                    <Wrapper>
+                      <Tippy
+                        placement="bottom"
+                        theme="light-border"
+                        interactive={true}
+                        onCreate={({popper}) => {
+                          popper.style.width = 'max-content';
+                        }}
+                        content="Level 4 (final)"
+                      >
+                        <Button>Level 3</Button>
+                      </Tippy>
+                    </Wrapper>
+                  }
+                >
+                  <Button>Level 2</Button>
+                </Tippy>
+              </Wrapper>
+            }
+          >
+            <Button>Level 1</Button>
+          </Tippy>
+        </Wrapper>
       }
     >
-      <Button>Text</Button>
+      <Button>Level 0</Button>
     </Tippy>
   );
 }
